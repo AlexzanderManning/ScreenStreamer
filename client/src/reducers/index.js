9cb3dd886525f935ee7315effe01d636a7bf0b70
AlexzanderManning <deonte94@icloud.com>
2019-12-11 17:39:03 -0500

    added redux forms, added create stream page with forms, and form validation

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+
 import authReducer from './auth-reducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
 
+///redux for reducer must have the form key name.
